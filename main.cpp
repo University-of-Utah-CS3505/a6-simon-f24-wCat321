8d5cfbdff3a03c6c82063d3e64677ce5aeacf158
321wCat <u1465594@utah.edu>
2025-10-28 17:13:12 -0600

    submission comments and demonstration video

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/**
+ * William Yount
+ * wCat321
+ *
+ * The creative element of my game is the "game swap" feature,
+ * where every few rounds, set to 5 currently, the positions of
+ * the blue and red buttons swap. this happens every 5 rounds.
+ * The colored button needed to press does not swap, if the pattern
+ * starts with "blue blue red" you will always need to press
+ * "blue blue red" even if the blue button is now on the other
+ * side.
+**/
 #include "mainwindow.h"
 
 #include <QApplication>
